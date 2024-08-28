fbe44011d9e9820f7966a0f68d65ce90fd55ad85
Azhan777 <azhan.ali@din.global>
2024-08-28 05:18:44 +0500

    feat: add necessary changes to complete POC

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,21 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "2.0.20"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+application {
+    mainClass.set("stargate")
 }
